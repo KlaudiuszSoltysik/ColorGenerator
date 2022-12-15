f49216b66f4089621b014d87fd48f59c880d14d2
Klaudiusz Sołtysik <klaudiusz.s1405@gmail.com>
2022-12-15 21:05:38 +0100

    15.12 work

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ var columns = Array.from(document.getElementsByClassName("col"));
 var empty = [];
 var images = [];
 
+//SPACEBAR PRESS EVENT HANDLING
+document.getElementsByTagName("body")[0].addEventListener("keydown", function(event) {
+    if (event.key == " " || event.code == "Space") {
+        randomizeColors();
+    }
+});
+
+//DISPLAYING ICONS ON HOVER
 for(let i = 0; i < columns.length; i++) {
     images.push(document.getElementById(`i${i}`));
 
@@ -14,6 +22,7 @@ for(let i = 0; i < columns.length; i++) {
     });
 }
 
+//HIDING COLUMNS
 for(let i = 3; i < 6; i++) {
     columns[i].style.display = "none";
     empty.push(columns[i]);
@@ -21,23 +30,77 @@ for(let i = 3; i < 6; i++) {
 
 var colors = columns.slice(0, 3);
 
+//SETTING INITIAL CONDITIONS
 setOnClick();
 randomizeColors();
 
+//FUNCTION SETS ONCLICK EVENT ON "ADD COLUMN" BUTTON
 function setOnClick() {
     for(let i = 0; i < colors.length; i++) {
-        colors[i].onclick = function() {
+        colors[i].getElementsByClassName("plus")[0].onclick = function() {
             addColumn(i);
         }
     }
 }
 
-function randomizeColors() {
-    for(let i = 0; i < colors.length; i++) {
-        colors[i].style.background = getRandomColor();
+//SHOWING/HIDING BUTTONS LOGIC
+function buttonsLogic() {
+    let plus = document.getElementsByClassName("plus");
+    if(colors.length > 5) {
+        for(let i = 0; i < colors.length; i++) {
+            plus[i].style.display = "none";
+        }
+    } else {
+        for(let i = 0; i < colors.length; i++) {
+            plus[i].style.display = "initial";
+        }
+    }
+
+    let minus = document.getElementsByClassName("minus");
+    if(colors.length > 3) {
+        for(let i = 0; i < colors.length; i++) {
+            minus[i].style.display = "initial";
+        }
+    } else {
+        for(let i = 0; i < colors.length; i++) {
+            minus[i].style.display = "none";
+        }
     }
 }
 
+//HANDLING "ADD COLUMN" BUTTON
+function addColumn(id) {
+    if(colors.length < 6) {
+        if(id == colors.length - 1) {
+            colors.push(empty.pop());
+            colors[id + 1].style.display = "initial";
+
+            colorize(colors[id + 1]);
+        } else {
+            colors.splice(id + 1, 0, empty.pop());
+            colors[id + 1].style.display = "initial";
+    
+            colors[id + 1].style.backgroundColor = colors[colors.length - 1].style.backgroundColor;
+            
+            for(let i = colors.length - 1; i > id + 1; i--) {
+                colors[i].style.backgroundColor = colors[i - 1].style.backgroundColor;
+            }
+
+            colorize(colors[id + 2]);
+        }
+        setOnClick();
+        buttonsLogic();
+    }
+}
+
+//RANDOMIZING COLORS FOR ALL ACTIVE AND NON-HIDDEN COLUMNS
+function randomizeColors() {
+    for(let i = 0; i < colors.length; i++) {
+        colorize(colors[i]);
+    }
+}
+
+//FUNCTION RETURNING RANDOM COLOR
 function getRandomColor() {
     let letters = '0123456789ABCDEF';
     let color = '#';
@@ -48,22 +111,31 @@ function getRandomColor() {
     return color;
 }
 
-function addColumn(id) {
-    if(id = colors.length - 1) {
-        colors.push(empty.pop());
-        colors[id + 1].style.display = "initial";
-        colors[id + 1].style.backgroundColor = getRandomColor();
+//COMPUTE IF COLOR IS LIGHT
+function isLight(color) {
+    const hex = color.replace('#', '');
+    const c_r = parseInt(hex.substring(0, 2), 16);
+    const c_g = parseInt(hex.substring(2, 4), 16);
+    const c_b = parseInt(hex.substring(4, 6), 16);
+    const brightness = ((c_r * 299) + (c_g * 587) + (c_b * 114)) / 1000;
+    if(brightness > 155) {
+        return true
     } else {
-        colors.splice(id + 1, 0, empty.pop());
-        colors[id + 1].style.display = "initial";
-        colors[id + 1].style.backgroundColor = colors[colors.length - 1].style.backgroundColor;
-        
-        for(let i = colors.length - 1; i > id + 1; i--) {
-            colors[i].style.backgroundColor = colors[i - 1].style.backgroundColor;
-        }
-
-        colors[id + 2].style.backgroundColor = getRandomColor();
+        return false
     }
+}
 
-    setOnClick(); 
+function colorize(column) {
+    let temp = getRandomColor()
+    column.style.backgroundColor = temp;
+
+    if(isLight(temp)) {
+        for(element of column.getElementsByTagName("img")) {
+            element.style.filter = "invert(0%) sepia(100%) saturate(29%) hue-rotate(133deg) brightness(93%) contrast(107%)";
+        }
+    } else {
+        for(element of column.getElementsByTagName("img")) {
+            element.style.filter = "invert(100%) sepia(0%) saturate(0%) hue-rotate(93deg) brightness(103%) contrast(103%)";
+        }
+    }    
 }
